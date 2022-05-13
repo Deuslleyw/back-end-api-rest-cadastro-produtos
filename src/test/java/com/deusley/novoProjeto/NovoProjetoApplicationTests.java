7f014a3f787cbbd95ee3067f2fd6bcd88518e5c7
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-05-13 18:23:44 -0300

    first commit

---
+++ src/test/java/com/deusley/novoProjeto/NovoProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.novoProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
