60cd75bbcea57aa1c4f27fbcf8d60832aaa6c6db
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-05-19 16:31:02 -0300

    ImplemetacaoBancoH2paraTestes

---
+++ src/main/java/com/deusley/novoProjeto/services/CategoriaService.java
@@ -0,0 +1,17 @@
+package com.deusley.novoProjeto.services;
+
+import org.springframework.stereotype.Service;
+
+import com.deusley.novoProjeto.domain.Categoria;
+import com.deusley.novoProjeto.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	private CategoriaRepository rep;
+	
+	public Categoria buscar() {
+		
+	}
+	
+}
