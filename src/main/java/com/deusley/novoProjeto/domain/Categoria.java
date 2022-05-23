b653a297b2ebc4516a23c76bf2e8a02d088f5eae
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-05-23 19:14:16 -0300

    Associaçao-de-tabelas-atualizaçao

--- src/main/java/com/deusley/novoProjeto/domain/Categoria.java
+++ src/main/java/com/deusley/novoProjeto/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.deusley.novoProjeto.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -19,6 +22,8 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy = "categorias")                           //  <<<<<<<<<Parei aqui, falta terminar o mapped.
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -50,7 +55,15 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
 
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -69,6 +82,9 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+	
+	}
 	
 	
-}
