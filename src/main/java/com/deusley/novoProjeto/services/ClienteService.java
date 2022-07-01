4a71c44aa69e5801fec61e9bd9da211853439fcb
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-07-01 03:16:02 -0300

    InsetNewCliente<Post>

--- src/main/java/com/deusley/novoProjeto/services/ClienteService.java
+++ src/main/java/com/deusley/novoProjeto/services/ClienteService.java
@@ -8,10 +8,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.deusley.novoProjeto.domain.Cidade;
 import com.deusley.novoProjeto.domain.Cliente;
+import com.deusley.novoProjeto.domain.Endereco;
+import com.deusley.novoProjeto.domain.enums.TipoCliente;
 import com.deusley.novoProjeto.dto.ClienteDTO;
+import com.deusley.novoProjeto.dto.ClienteNewDTO;
 import com.deusley.novoProjeto.repositories.ClienteRepository;
+import com.deusley.novoProjeto.repositories.EnderecoRepository;
 import com.deusley.novoProjeto.services.Exceptions.DataIntegrityExcepition;
 import com.deusley.novoProjeto.services.Exceptions.ObjectNotFoundExcepition;
 
@@ -20,12 +26,26 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository rep;
+	
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 
 	public Cliente find(Integer id) {
 		return rep.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundExcepition(" Cliente  de ID: " + id + ", não encontrado!"));
 	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);                                                  
+		obj = rep.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	
+	}
+	
 	public Cliente update(Cliente obj) {
 		Cliente newObj =  find(obj.getId());
 		updateData(newObj, obj);
@@ -53,6 +73,20 @@ public class ClienteService {
     return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(),null , null);
     }
     
+    public Cliente fromDTO(ClienteNewDTO objDTO) {
+    	
+    Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+    Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+    Endereco end = new Endereco(null, objDTO.getLogradouro(),objDTO.getNumero(),objDTO.getComplemento(),objDTO.getBairro(),objDTO.getCep(), cli, cid);
+   
+    cli.getEnderecos().add(end)	;
+    cli.getTelefones().add(objDTO.getTelefone1());
+    if (objDTO.getTelefone2() != null) {
+    	cli.getTelefones().add(objDTO.getTelefone2());
+    }
+    return cli;
+    }
+    
     private void updateData(Cliente newObj,Cliente obj) {
     	newObj.setNome(obj.getNome());
     	newObj.setEmail(obj.getEmail());
@@ -61,3 +95,4 @@ public class ClienteService {
 }
 
 
+//DeusleyDiego//API0030
