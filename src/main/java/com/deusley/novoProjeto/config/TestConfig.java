75cf2dcdfd449dc20278d8977c842af3019ccf29
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-07-13 00:15:32 -0300

    prod-configs-bdc

--- src/main/java/com/deusley/novoProjeto/config/TestConfig.java
+++ src/main/java/com/deusley/novoProjeto/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.deusley.novoProjeto.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,8 +17,15 @@ public class TestConfig {
 	@Autowired
 	private DBService dbsService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbsService.instantiateTestDatabase();
 		return true;
 	}
